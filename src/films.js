9e13fe4647e4193587aeeda9cf42cf2d9eca6f92
Juan Trujillo <juantrujillobrinas@gmail.com>
2022-05-09 13:00:59 +0200

    Exercise 4

--- src/films.js
+++ src/films.js
@@ -8,23 +8,35 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  let directors = array.filter(movie => movie.director === director)
+  let directors = array.filter(movies => movies.director === director);
   return directors;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  let meanMovies = array.reduce()
+  let directors = array.filter(movies => movies.director === director);
+  let meanScoreFilms = directors.reduce((acc, item) => acc.score + item.score); //Mejorar / cambiar.
+  let scoreAverage= parseFloat((meanScoreFilms / directors.length).toFixed(2));
+  return scoreAverage;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let titles = array.map(movies => movies.title);
+  let orderedMoviesAZ = titles.sort().slice(0,20);
+  return orderedMoviesAZ;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  array.map(movies => {
+    let orderedFilmsYear = movies.year.sort((a,b) =>(a - b));
+    console.log(orderedFilmsYear);
+  });
+  
+  
+  
+  return orderedFilmsYear;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
@@ -39,7 +51,9 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+  let moviesForYear = array.filter(movies => movies.year === year);
+  let orderedMovies = moviesForYear.sort((a,b) => (a.score > b.score) ? 1 : -1);
+  let bestFilmOfYear = orderedMovies
 }
 
 
